20576185186c8bd7bb6971688caa63dad84de01d
Junior Valera <detoprox@gmail.com>
2024-11-13 16:48:08 -0400

    subcadenas en JavaScrip

---
+++ funciones incorporadas en javascript/subcadenasEnJs.js
@@ -0,0 +1,9 @@
+// subcadenas en javaScript
+
+let cadena3 = "Hola mundo ";
+
+// substring (indice_inicio indice_fin -1)
+// no se puede estraer el indice fin de la cadena
+
+let cadena1 = cadena3.substring(5,10);
+console.log(cadena1);
