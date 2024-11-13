1fe6c9ff4bec5a3c8c49dc1c65dbf83f08caa683
Junior Valera <detoprox@gmail.com>
2024-11-13 17:28:33 -0400

    Convertir un Numero en Cadena

---
+++ funciones incorporadas en javascript/DeNumeroACadena.js
@@ -0,0 +1,7 @@
+// Convertir de numero a cadena
+let a = 20, b = 10; // si son valores numericos se summan
+let suma = a + b;
+console.log(suma);
+// Convertir de numero a cadena con un metodo
+let cadena = a.toString() + b.toString();
+console.log(cadena);// conmo se convirtio en cadena se unen los valores y no se suman
