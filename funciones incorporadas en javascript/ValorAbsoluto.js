c9bd1089faeaf600e6f9ab275f5b988e88880985
Junior Valera <detoprox@gmail.com>
2024-11-13 17:43:21 -0400

    Valor absoluto de un numero con la clase Math.abs(Mivariable)

---
+++ funciones incorporadas en javascript/ValorAbsoluto.js
@@ -0,0 +1,6 @@
+// valor absoluto de un numero en javaScript
+
+let numero = -10; // el valor absoluto de un numero es el mismo numero sin importar su signo siempre sera positivo
+
+let ValorAnsoluto = Math.abs(numero);
+console.log(ValorAnsoluto);// aqui se ve que aunque el valor es negativo la salida es positiva
