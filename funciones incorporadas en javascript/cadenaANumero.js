942bda9f7da9894691079044878d648cc36af356
Junior Valera <detoprox@gmail.com>
2024-11-13 17:12:17 -0400

    convertir cadenas a numeros en JavaScript

---
+++ funciones incorporadas en javascript/cadenaANumero.js
@@ -0,0 +1,8 @@
+// Convertir de cadenas a numeros en JavaScript 
+ let a = "10", b = "20";
+// Si son cadenas se concatenan 
+ console.log(a+b);
+// Convertir de cadena a numero con un metodo
+
+let suma = parseInt(a) + parseInt(b);
+console.log(suma);
