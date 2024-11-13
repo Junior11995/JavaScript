136f57505886551c36a2ed09f87f77e79aa665bc
Junior Valera <detoprox@gmail.com>
2024-11-13 17:02:12 -0400

    Concatenacion de cadenas en JavaScript

---
+++ funciones incorporadas en javascript/concatenacionDeCadenas.js
@@ -0,0 +1,12 @@
+// Concatenacion de cadenas en JavaScript
+ 
+let cadena1 = "Hola";
+let cadena2 = "Mundo";
+let cadena3 = cadena1 + " " + cadena2;
+
+console.log(cadena3);
+
+// Concatenacion con string Interpolation
+let cadena4 = `${cadena1} ${cadena2}`
+
+console.log(cadena4);
